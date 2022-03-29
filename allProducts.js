6647f097b17325c7f243bd579f588712c8718467
wisestme01 <wyhzest@gmail.com>
2022-03-29 10:58:09 +0100

    first commit

---
+++ allProducts.js
@@ -0,0 +1,65 @@
+
+// cart
+let cart = []
+
+// getting the products
+class Products{
+  async getProducts(){
+    try {
+      let result = await fetch('productList.json');
+      
+      let data = await result.json();
+      let products = data;
+      products = products.map(item => {
+        const {id,name,price,description,details,image} = item;
+        console.log(image, name);
+        return {id, name, price, description, details, image};
+      })
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+// display products
+let productsContainer = document.querySelector('.products_container');
+class UI {
+  displayProducts(products) {
+    let result = '';
+    products.forEach(product => {
+      
+      result += `
+      <div class="swiper-slide">
+								<div class="product-single">
+									<div class="product-image">
+										<figure>
+											<img src="./assets/images/cream${product.id}.jpg" alt="" />
+										</figure>
+									</div>
+									
+									<div class="product-information">
+										<h3>${product.name} <a href="#">Cosmetic</a></h3>
+										<div class="product_price">
+                    &#8358;${product.price}
+										</div>
+									</div>
+								</div>
+							</div>
+      `
+    });
+    productsContainer.innerHTML = result;
+  }
+}
+
+// local storage
+class Storage {
+
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  const ui = new UI();
+  const products = new Products();
+
+  // get all products
+  products.getProducts().then(products => ui.displayProducts(products));
+})
---
+++ assets/js/main.js
@@ -0,0 +1,196 @@
+(function ($) {
+    "use strict";
+	
+	var $window = $(window); 
+	var $body = $('body'); 
+	
+	/* Preloader Effect */
+	$window.load(function() {
+		$(".preloader").fadeOut(600);
+    });
+	
+	/* Sticky header */
+	$window.scroll(function(){
+    	if ($window.scrollTop() > 200) {
+			$('.navbar').addClass('sticky-header');
+		} else {
+			$('.navbar').removeClass('sticky-header');
+		}
+	});
+	
+	/* Our Client slider */
+	var swiper = new Swiper('.testimonial-slider', {
+		grabCursor: true,
+		speed: 1500,
+		autoplay: true,
+		slidesPerView: 2,
+		spaceBetween: 30,
+		breakpoints: {
+			480: {
+				slidesPerView: 1
+			},
+			
+			768: {
+				slidesPerView: 2
+			}
+		},
+		pagination: {
+			el: '.testimonial-pagination',
+			type: 'bullets',
+			clickable: true
+		},
+	});
+
+	/* Product slider */
+	var swiper = new Swiper('.product-slider', {
+		grabCursor: true,
+		speed: 1500,
+		autoplay: true,
+		slidesPerView: 4,
+		spaceBetween: 30,
+		breakpoints: {
+			480: {
+				slidesPerView: 1
+			},
+			
+			768: {
+				slidesPerView: 2
+				
+			},
+			
+			991: {
+				slidesPerView: 3
+				
+			}
+		},
+		pagination: {
+			el: '.categories_pagination',
+			clickable: true
+		},
+		// navigation: {
+		// 	nextEl: '.swiper-button-next',
+		// 	prevEl: '.swiper-button-prev',
+		// 	},
+	});
+	
+	/* Category slider */
+	var swiper = new Swiper('.category_slider', {
+		grabCursor: true,
+		speed: 1500,
+		autoplay: true,
+		slidesPerView: 4,
+		spaceBetween: 30,
+		breakpoints: {
+			480: {
+				slidesPerView: 1
+			},
+			
+			768: {
+				slidesPerView: 2
+				
+			},
+			
+			991: {
+				slidesPerView: 3
+				
+			}
+		},
+		pagination: {
+			el: '.categories_pagination',
+			clickable: true
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+			},
+	});
+	
+	
+	/* Offers slider */
+	var swiper = new Swiper('.latest_products', {
+		effect: 'flip',
+		speed: 1500,
+		autoplay: true,
+		grabCursor: true,
+		cubeEffect: {
+			shadow: false
+		},
+		pagination: {
+			el: '.latest_products_pagination',
+			clickable: true
+		},
+	});
+	
+	/* Header slider */
+	var swiper = new Swiper('.header-slider', {
+		allowSwipeToPrev: false,
+		grabCursor: true,
+		autoplay: true,
+		speed: 1500,
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+		},
+		navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+		},
+	});
+	
+	/* Contact form validation */
+	var $contactform=$("#contactForm");
+	$contactform.validator({focus: false}).on("submit", function (event) {
+		if (!event.isDefaultPrevented()) {
+			event.preventDefault();
+			submitForm();
+		}
+	});
+
+	function submitForm(){
+		/* Initiate Variables With Form Content*/
+		var name = $("#name").val();
+		var email = $("#email").val();
+		var subject = $("#subject").val();
+		var message = $("#message").val();
+
+		$.ajax({
+			type: "POST",
+			url: "form-process.php",
+			data: "name=" + name + "&email=" + email + "&subject=" + subject + "&message=" + message,
+			success : function(text){
+				if (text == "success"){
+					formSuccess();
+				} else {
+					submitMSG(false,text);
+				}
+			}
+		});
+	}
+
+	function formSuccess(){
+		$contactform[0].reset();
+		submitMSG(true, "Message Sent Successfully!");
+	}
+
+	function submitMSG(valid, msg){
+		if(valid){
+			var msgClasses = "h3 text-success";
+		} else {
+			var msgClasses = "h3 text-danger";
+		}
+		$("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+	}
+	/* Contact form validation end */
+	
+	/* Animate with wow js */
+    new WOW({mobile:false}).init(); 
+	
+	/* Parallaxie Js */
+	if ($(window).width() > 768) {
+		$('.parallaxie').parallaxie({
+			speed: 0.55,
+			 offset: 0,
+		});
+	}
+})(jQuery);
+
